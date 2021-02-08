89aa57cef7e68abf0791e1d9d8acad933da2ca07
Jozef Tran <263396@uwr.edu.pl>
2021-02-08 13:47:05 +0100

    added missing requirements typos different fetching way for general statistics

--- build_db/scrape_data.py
+++ build_db/scrape_data.py
@@ -12,20 +12,58 @@ import operator
 from django.db.models import Q
 
 
+def fetch_PDB_codes(date):
+    date = date.strftime('%Y-%m-%dT%H:%M:%S')
+    """Fetchs PDB codes for all structures with metal ion in them.
+    If the response returned has an 500 error code, an error will
+    be comunicated. """
+    string = '''
+    {
+    "query": {
+        "type": "group",
+        "logical_operator": "and",
+        "nodes": [
+        {
+            "type": "terminal",
+            "service": "text",
+            "parameters": {
+            "operator": "less",
+            "value":"'''+str(date)+'''",
+            "attribute": "rcsb_accession_info.initial_release_date"
+          }
+        }
+      ]
+    },
+    "request_options": {
+        "return_all_hits": true
+    },
+    "return_type": "entry"
+    }'''
+
+    query_string= json.loads(string)
+    url = 'http://search.rcsb.org/rcsbsearch/v1/query'
+
+    response = requests.post(url,json=query_string)
+
+    if response.status_code == 200: #request has succeeded
+        jsonresponse = response.json()
+        return [jsonresponse['result_set'][code]['identifier'] for code in range(jsonresponse['total_count'])]
+
+    raise Exception("No fetched codes from RCSB.")
+
+
 
 with_metals_no_interface = Pdb.objects.filter(has_metal_interface=False).count()
 with_metal_interface = Pdb.objects.filter(has_metal_interface=True).count()
 database_count = Pdb.objects.all().count()
 newest_deposition_date = Pdb.objects.latest("deposition_date").deposition_date
 
-r = requests.post("https://www.rcsb.org/pdb/search/smartRow.do",
-headers={"Content-Type": "application/x-www-form-urlencoded"},
-data={"smartSearchSubtype": "ReleaseDateQuery", "r": "0", "target": "Current",
-"pdbx_audit_revision_history.revision_date.comparator": "between",
-"pdbx_audit_revision_history.revision_date.max": newest_deposition_date,
-})
 
-pdb_count = int(r.text.split("'")[1].split()[0])
+
+
+
+
+pdb_count = len(fetch_PDB_codes(newest_deposition_date))
 no_metal = int(pdb_count) - int(database_count)
 update_date = datetime.date.today()
 #save results to the database
